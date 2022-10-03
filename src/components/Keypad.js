3ce6402778d17171e75372279371e751999120b4
kimuit117 <kemoikiuita@gmail.com>
2022-10-03 04:44:00 +0300

    lab passed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
     return (
-        <div></div>
-    )
+        <div>
+            <input 
+            type="password" 
+            onChange={() => console.log('Entering password...')}></input>
+        </div>
+    );
 }
 
 export default Keypad;
